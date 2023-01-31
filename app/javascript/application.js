4441b88c508b882f8d24f61c823bf692fabdba12
naokiiwasa8 <naoki.iwasa8@gmail.com>
2023-02-01 00:02:07 +0900

    toast success -> esbuild by jsbundling-rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,4 +2,6 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+// 読み込めない
 import "popper"
--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
+// hello_controller.js
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+  static targets = [ "name", "output" ]
+
+  greet() {
+    this.outputTarget.textContent =
+      `Hello, ${this.nameTarget.value}!`
   }
 }
+
+
+// export default class extends Controller {
+//   connect() {
+//     this.element.textContent = "Hello World!"
+//   }
+// }
