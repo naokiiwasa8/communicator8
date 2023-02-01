ed3418a6c61fdc0ce96dc313b157ed8ba00846af
naokiiwasa8 <naoki.iwasa8@gmail.com>
2023-02-01 23:56:49 +0900

    add gem ransack

--- Gemfile
+++ Gemfile
@@ -60,6 +60,9 @@ gem 'simple_form'
 # page_nation
 gem "kaminari"
 
+# search 
+gem "ransack"
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,10 @@ class HomeController < ApplicationController
   def index
     @communities = Community.all.order(created_at: :desc)
     @communities = Community.page(params[:page])
+    # ransack search
+    @search = Community.ransack(params[:q])
+    @search.sorts = 'created_at desc' if @search.sorts.empty? 
+    @communities = @search.result.page(params[:page])
   end
 end
 
