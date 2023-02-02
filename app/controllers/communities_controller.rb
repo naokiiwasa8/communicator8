db1d5050834ba70ebf69837cf47640fd6ea3d7b1
naokiiwasa8 <naoki.iwasa8@gmail.com>
2023-02-02 22:00:28 +0900

    fix ransack

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -4,6 +4,11 @@ class CommunitiesController < ApplicationController
   # GET /communities or /communities.json
   def index
     @communities = Community.all.order(created_at: :desc)
+    @communities = Community.page(params[:page])
+    # ransack search
+    @search = Community.ransack(params[:q])
+    @search.sorts = 'created_at desc' if @search.sorts.empty? 
+    @communities = @search.result.page(params[:page])
   end
 
   # GET /communities/1 or /communities/1.json
