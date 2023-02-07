862506ea30bfb5b621b2aeb126d920d1692cb953
naokiiwasa8 <naoki.iwasa8@gmail.com>
2023-02-07 23:52:45 +0900

    fix

--- app/javascript/controllers/toast.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="toast"
-export default class extends Controller {
-  connect() {
-    new bootstrap.Toast(this.element, { delay: 5000 }).show()
-  }
-}
