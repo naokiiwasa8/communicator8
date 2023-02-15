a6f4ac7feaf28a2e084cb926a09c0472f61196a0
naokiiwasa8 <naoki.iwasa8@gmail.com>
2023-02-16 00:46:40 +0900

    top view fixed by communicator SVG

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,15 @@ module ApplicationHelper
   def turbo_stream_flash
     turbo_stream.append "toasts", partial: "toasts"
   end
+
+  # svgファイル表示
+  def embedded_svg(filename, options={})
+    file = File.read(Rails.root.join('app', 'assets', 'images', filename))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    if options[:class].present?
+      svg['class'] = options[:class]
+    end
+    doc.to_html.html_safe
+end
 end
