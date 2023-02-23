c974db0f5ba0d43e7c3c0e0b178696e112d28389
naokiiwasa8 <naoki.iwasa8@gmail.com>
2023-02-24 00:17:04 +0900

    communityの回答ユーザー人数のカウントと、user_nameをsign_upに入れれるように追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  # User登録のカスタマイズ（user_nameを登録できるように追加）
+  def configure_permitted_parameters
+    added_attrs = [ :email, :user_name, :password, :password_confirmation ]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
 end
--- app/models/community.rb
+++ app/models/community.rb
@@ -3,10 +3,13 @@ class Community < ApplicationRecord
   ## Relationship
   has_many :posts
   belongs_to :user, foreign_key: "user_id"
+  # communituyに対して投稿したUserの人数を取得
+  has_many :posting_users, through: :posts, source: :user
 
   ## Validations
   with_options presence: true do
     validates :community_name
   end
+  
   
 end
