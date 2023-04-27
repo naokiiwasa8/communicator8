57375f8412295046d7f546ecc24cc676f9bf5eaf
naokiiwasa8 <naoki.iwasa8@gmail.com>
2023-04-28 01:00:27 +0900

    toast uodate

--- app/javascript/controllers/toast_controller.js
+++ app/javascript/controllers/toast_controller.js
@@ -3,6 +3,33 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="toast"
 export default class extends Controller {
   connect() {
-    new bootstrap.Toast(this.element, { delay: 5000 }).show()
+    this.element.style.transform = 'translateY(-100%)';
+    setTimeout(() => {
+      this.slideIn();
+    }, 10);
+
+    const toast = new bootstrap.Toast(this.element, { delay: 3000, autohide: false });
+    toast.show();
+
+    setTimeout(() => {
+      this.slideOut(() => {
+        toast.hide();
+      });
+    }, 5000);
+  }
+
+  slideIn() {
+    this.element.style.transition = 'transform 0.5s ease-in-out';
+    this.element.style.transform = 'translateY(0)';
+  }
+
+  slideOut(callback) {
+    this.element.style.transition = 'transform 0.5s ease-in-out';
+    this.element.style.transform = 'translateY(-200%)';
+    setTimeout(() => {
+      if (callback) {
+        callback();
+      }
+    }, 1000);
   }
 }
