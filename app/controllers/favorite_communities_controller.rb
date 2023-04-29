700693978662755219567922a87f3bd93fecfd90
naokiiwasa8 <naoki.iwasa8@gmail.com>
2023-04-29 20:06:30 +0900

    fix

--- app/controllers/favorite_communities_controller.rb
+++ app/controllers/favorite_communities_controller.rb
@@ -1,16 +1,31 @@
 class FavoriteCommunitiesController < ApplicationController
+  before_action :set_communtiy
+  before_action :check_auth
+
   def create
-    @community = Community.find(params[:community_id])
     current_user.favorite_communities.create(community_id: @community.id)
     flash.now.notice = "お気に入りに追加しました"
     render "result"
   end
 
   def destroy
-    @community = Community.find(params[:community_id])
     community_favorite = current_user.favorite_communities.find_by(community_id: @community.id)
     community_favorite.destroy!
     flash.now.notice = "お気に入りから削除しました"
     render "result"
+  end
+
+  private
+
+  def set_communtiy
+    @community = Community.find(params[:community_id])
+  end
+
+  def check_auth
+    unless current_user
+      flash.now.notice = "ログイン後にお気に入り登録ができます"
+      render "result"
+      return
+    end
   end
 end
