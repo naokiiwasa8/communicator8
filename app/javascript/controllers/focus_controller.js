d1d445b50e3511ff572dd76f13934a9c1bcc5bd7
naokiiwasa8 <naoki.iwasa8@gmail.com>
2023-05-10 23:26:34 +0900

    fix focus controller

--- app/javascript/controllers/focus_controller.js
+++ app/javascript/controllers/focus_controller.js
@@ -10,5 +10,12 @@ export default class extends Controller {
 
   focusInput() {
     this.inputTarget.focus();
+
+    // キーボードを開くためのキーボードイベントを発生させる
+    const event = new KeyboardEvent('keydown', {
+      bubbles: true,
+      keyCode: 229
+    });
+    this.inputTarget.dispatchEvent(event);
   }
 }
