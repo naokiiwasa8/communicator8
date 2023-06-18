434aefaf0b6bd00f63b852d914114c2176dbd9fa
naokiiwasa8 <naoki.iwasa8@gmail.com>
2023-06-19 02:26:20 +0900

    slack fix

--- app/models/concerns/slack_notifier.rb
+++ app/models/concerns/slack_notifier.rb
@@ -4,8 +4,9 @@ class SlackNotifier
   # 環境SLACK_WEBHOOK_URLにwebhook urlを格納
   WEBHOOK_URL = ENV['SLACK_WEBHOOK_URL']
   CHANNEL = "#communicator_app"
+  USER_NAME = "communicator"
 
   def send(message)
-    Slack::Notifier.new(WEBHOOK_URL, channel: CHANNEL).ping('message')
+    Slack::Notifier.new(WEBHOOK_URL, channel: CHANNEL, username: USER_NAME).ping(message)
   end
 end
